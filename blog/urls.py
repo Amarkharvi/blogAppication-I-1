161e3c526c4bc462615468018920151762dc35da
Amarkharvi <amarkharvi316@gmail.com>
2020-10-26 13:43:56 +0530

    First Commit on my Internship Project

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from blog.views import PostListView, post_share
+from django.urls.conf import path
+from django.urls import path
+from . import views
+
+app_name='blog'
+
+urlpatterns=[
+    #path('',views.post_list,name="post_list"),
+    path('',views.PostListView.as_view(),name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail,name="post_detail",),
+    path('<int:post_id>/share/',views.post_share,name='post_share')
+]
